84329a0816292f7a4120b0995296722de16c2d8f
Riru233 <1125149887@qq.com>
2026-03-12 22:28:52 +0800

    vFluent3不兼容，移除

--- src/window/settings/index.vue
+++ src/window/settings/index.vue
@@ -1,8 +1,6 @@
 <template>
-  <LiquidWin :width="950" :height="650" 
-    :displacementScale="78" v-model:top="top" v-model:left="left"
-    @updatePos="updatePos"
-    :active="props.active" v-slot="{ startDrag }">
+  <LiquidWin :width="950" :height="650" :displacementScale="78" v-model:top="top" v-model:left="left"
+    @updatePos="updatePos" :active="props.active" v-slot="{ startDrag }">
     <div class="glass-content" style="display: flex; flex-direction: column; flex: 1;">
 
       <div class="titlebar" @mousedown="startDrag">
